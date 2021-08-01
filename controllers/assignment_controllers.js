d9875f54b105dd6cff2f92ead112dc58e007e8b3
johndwatt <jdw23@protonmail.com>
2021-08-01 11:39:50 -0700

    adds working new route outline to assignment_controllers

--- controllers/assignment_controllers.js
+++ controllers/assignment_controllers.js
@@ -8,4 +8,9 @@ router.get("/", function (req, res) {
     res.send("index route works");
 });
 
+//new
+router.get("/new", function (req, res) {
+    res.send("new route works");
+});
+
 module.exports = router;
