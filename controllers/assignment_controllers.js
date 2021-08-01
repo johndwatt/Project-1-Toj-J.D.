dbf762e2d7966b52559fd3818a4a4c9be5906c37
johndwatt <jdw23@protonmail.com>
2021-08-01 12:15:30 -0700

    adds working delete route outline to assignment_controllers and edits routes to be arrow functions

--- controllers/assignment_controllers.js
+++ controllers/assignment_controllers.js
@@ -4,17 +4,17 @@ const router = express.Router();
 const { Assignment } = require("../models");
 
 //index
-router.get("/", function (req, res) {
+router.get("/", (req, res) => {
     res.send("index route works");
 });
 
 //new
-router.get("/new", function (req, res) {
+router.get("/new", (req, res) => {
     res.send("new route works");
 });
 
 //create
-router.post("/", function(req, res) {
+router.post("/", (req, res) => {
     res.send({
         message: "create route works",
         body: req.body,
@@ -22,20 +22,28 @@ router.post("/", function(req, res) {
 });
 
 //show
-router.get("/:id", function (req, res) {
+router.get("/:id", (req, res) => {
     res.send(`show route works with id: ${req.params.id}`);
 });
 
 //edit
-router.get("/:id/edit", function (req, res) {
+router.get("/:id/edit", (req, res) => {
     res.send(`edit route works with id: ${req.params.id}`);
 });
 
 //update
-router.put("/:id", function (req, res) {
+router.put("/:id", (req, res) => {
     res.send({
         message: "update route works",
         body: req.body,
+        id: req.params.id,
+    });
+});
+
+//destroy
+router.delete("/:id", (req, res) => {
+    res.send({
+        message: "delete route works",
         id: req.params.id,
     });
 });
