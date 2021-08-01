20c7d0e9ffdc9c7666ea9b939aa88e2dd671d1bb
johndwatt <jdw23@protonmail.com>
2021-08-01 12:48:57 -0700

    adds login route outline

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const { User } = require("../models");
 
+//index - login
+router.get("/login", (req, res) => {
+    res.send("login route works");
+});
 
 
 module.exports = router;
