f443b047bde3178c92a965c5159503752608e4de
johndwatt <jdw23@protonmail.com>
2021-08-01 12:49:34 -0700

    adds signup route outline

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -8,5 +8,8 @@ router.get("/login", (req, res) => {
     res.send("login route works");
 });
 
+router.get("/signup", (req, res) => {
+    res.send("signup route works");
+});
 
 module.exports = router;
