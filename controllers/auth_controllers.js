7b5fe5d5e435768300af309aba009c84f5a7a6ce
johndwatt <jdw23@protonmail.com>
2021-08-02 19:59:01 -0700

    adds signup post route to auth_controllers

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -16,8 +16,21 @@ router.post("/login", (req, res) => {
     res.send("login post route works");
 });
 
-router.post("/signup", (req, res) => {
-    res.send("signup post route works");
+router.post("/signup", async (req, res) => {
+    try {
+        const foundUser = await User.exists({ email: req.body.email });
+        if (foundUser) {
+            return res.redirect("/login");
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hash;
+        const createdUser = await User.create(req.body);
+        return res.redirect("/login");
+    } catch (error){
+        console.log(error);
+        return res.send(error);
+    }
 });
 
 router.get("/logout", (req, res) => {
