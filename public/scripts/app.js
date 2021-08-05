a22719c676cce5738a2f53d92789df8201624a48
johndwatt <jdw23@protonmail.com>
2021-08-05 11:42:53 -0700

    adds theme functionality and button to app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,21 @@
         $('.ui.labeled.icon.sidebar').sidebar('toggle');
     });
 
+// Themes
+
+$('#theme-selector').on("change", (e) => {
+    $("html").css("--primary", e.target.value);
+    let theme = e.target.value;
+    localStorage.setItem("theme", theme);
+});
+const currentTheme = localStorage.getItem("theme");
+
+function checkTheme(){
+    $("html").css("--primary", currentTheme);
+}
+
+window.onload = checkTheme();
+
 // filepond
 
 // Get a reference to the file input element
